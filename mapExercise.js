3561ffe875b8da05d588ac34c0b72976cbc8200c
MuhsinADA <muhsin.muadth@ada.ac.uk>
2025-01-15 10:30:18 +0000

    refined

--- mapExercise.js
+++ mapExercise.js
@@ -26,7 +26,7 @@ function numberOfBooks() {
 addBook("12345", "The Lord of the Rings", "J.R.R. Tolkien");
 addBook("67890", "Pride and Prejudice", "Jane Austen");
 
-console.log(findBook("12345")); // Should output the book object
-removeBook("12345");
+// console.log(findBook("12345")); // Should output the book object
+// removeBook("12345");
 listAllBooks(); // Should list the remaining book(s)
-console.log(numberOfBooks()); // Should output the correct count
+// console.log(numberOfBooks()); // Should output the correct count
