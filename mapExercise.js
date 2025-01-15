abcd16d4cdcbd3ebb31095f642f05ae83b56b396
MuhsinADA <muhsin.muadth@ada.ac.uk>
2025-01-15 10:17:00 +0000

    completed solution

--- mapExercise.js
+++ mapExercise.js
@@ -1,28 +1,31 @@
+javascript
 const catalogue = new Map();
 
 function addBook(bookId, title, author) {
-  // TODO: Implement this function to add a book to the catalogue.
+  catalogue.set(bookId, { title, author });
 }
 
 function findBook(bookId) {
-  // TODO: Implement this function to find a book by its ID.
+  return catalogue.get(bookId);
 }
 
 function removeBook(bookId) {
-  // TODO: Implement this function to remove a book from the catalogue.
+  catalogue.delete(bookId);
 }
 
 function listAllBooks() {
-  // TODO: Implement this function to list all books in the catalogue.
+  for (const [bookId, book] of catalogue.entries()) {
+    console.log(`ID: ${bookId}, Title: ${book.title}, Author: ${book.author}`);
+  }
 }
 
 function numberOfBooks() {
-  // TODO: Implement this function to return the number of books in the catalogue.
+  return catalogue.size;
 }
 
 // Test the functions
 addBook("12345", "The Lord of the Rings", "J.R.R. Tolkien");
-addBook("67890", "Pride and Prejudice", "Jane Austen"); 
+addBook("67890", "Pride and Prejudice", "Jane Austen");
 
 console.log(findBook("12345")); // Should output the book object
 removeBook("12345");
